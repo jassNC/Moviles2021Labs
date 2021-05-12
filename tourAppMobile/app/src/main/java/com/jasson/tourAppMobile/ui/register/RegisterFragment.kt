e8f2f5d387f96ecbbc0deff8476da638deeee775
joriheso <joriheso@gmail.com>
2021-05-12 14:59:28 -0600

    ajustes register y scroll

--- tourAppMobile/app/src/main/java/com/jasson/tourAppMobile/ui/register/RegisterFragment.kt
+++ tourAppMobile/app/src/main/java/com/jasson/tourAppMobile/ui/register/RegisterFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.jasson.tourAppMobile.R
 import com.jasson.tourAppMobile.ui.profile.ProfileViewModel
+import java.util.*
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+
 }
